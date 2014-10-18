191a6e4b85e1008798a09fb27ffdb684f643811e
barrypearseburke <barry.burke3@mydit.ie>
2014-10-18 21:37:47 +0100

    not printing in idel
    printing command line windows

--- Income-predictor.py
+++ Income-predictor.py
@@ -10,16 +10,18 @@
 # Take in file from web. Split the file 60% training set and 40% predict.
 #Take the train part and divide it into people who make over $50k and under or equal to $50k
 
-#data file comes from http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data.
+#data file comes from http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data
 
 import string
 import httplib2
 
 
 #vars that hold url
-filedown= "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data."
+filedown= "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
 h = httplib2.Http(".cache")
 file_headers, file =h.request(filedown)
 file=file.decode().split("\n")
+
+print (file)
 
 
