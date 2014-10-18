1950f7978ea38ce916ee5e0b82bd4156e8c8f09e
barrypearseburke <barry.burke3@mydit.ie>
2014-10-18 16:04:55 +0100

    Income Predictor
    only comments

---
+++ Income-predictor.py
@@ -0,0 +1,14 @@
+#OOP Dt211/2 Assignment
+# Due Date 30th of Novemeber
+#Date Given 16th of October 2014
+#Title Income Predictor
+#Author: Barry Burke
+#Student Number :C13427078
+#----------------------------------
+
+
+# Take in file from web. Split the file 60% training set and 40% predict.
+#Take the train part and divide it into people who make over $50k and under or equal to $50k
+
+#data file comes from http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data.
+
