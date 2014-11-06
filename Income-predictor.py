9b28e37b07d371d06fc3b3ec2cfbdd09a83123e4
barrypearseburke <barry.burke3@mydit.ie>
2014-11-06 15:33:50 +0000

    dictionaries defined

--- Income-predictor.py
+++ Income-predictor.py
@@ -1,6 +1,6 @@
-#OOP Dt211/2 Assignment
+# OOP Dt211/2 Assignment
 # Due Date 30th of November
-#Date Given 16th of October 2014
+# Date Given 16th of October 2014
 #Title Income Predictor
 #Author: Barry Burke
 #Student Number :C13427078
@@ -19,30 +19,32 @@ import string
 #download fxn.
 def downloadfile():
     #vars that hold url
-    filedown= "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+    filedown = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
     h = httplib2.Http(".cache")
-    file_headers, file =h.request(filedown)# that is working correctly . Header isent being added to file.
+    file_headers, file = h.request(filedown)  # that is working correctly . Header isent being added to file.
     file = file.decode()
     file = file.split('\n')
     for line in file:
-        print (line)
+        print(line)
     return file
+
 
 #find out how many entries are in file
 def lines(file):
-    linecounter=0
+    linecounter = 0
     for line in file:
-        linecounter +=1
-    return(int(linecounter))
+        linecounter += 1
+    return (int(linecounter))
+
 
 #split file downloaded into training and testing. Store in a file
 def splitfile(file):
-
     #will ask the user how much of the file they want to use for training and how much for testing.
-    print("How much of the file is to be used for training , precentage.\nThe precntage must be greater than 30 and less than 75")
+    print(
+        "How much of the file is to be used for training , precentage.\nThe precntage must be greater than 30 and less than 75")
 
-    while(1==1):
-        precentage =0
+    while (1 == 1):
+        precentage = 0
         precentage = float(input("\n:"))
         ##have to make precentage##
 
@@ -54,37 +56,38 @@ def splitfile(file):
 
     try:
         #declare 2 files - training and testing
-        training = open("trainfile.txt","w")#opens and creates a file called trainfile.txt in write mode
+        training = open("trainfile.txt", "w")  #opens and creates a file called trainfile.txt in write mode
     except IOError as etraining:
-        print(etraining)#gives error to user if cant open
+        print(etraining)  #gives error to user if cant open
         quit()
 
     try:
-        test = open ("test.txt","w") #opens and creates a file called test .txt in write mode
+        test = open("test.txt", "w")  #opens and creates a file called test .txt in write mode
     except IOError as etest:
-        print(etest)#gives error to user if cant open
+        print(etest)  #gives error to user if cant open
         quit()
 
     NumberOfLines = lines(file)
-    print(NumberOfLines) #32563 lines
+    print(NumberOfLines)  #32563 lines
 
-    counter=0
+    counter = 0
     #find out how many lines go to each file
 
-    precentage =precentage/100
-    TrainingEntries = (NumberOfLines*precentage)
-    TestingEntries = NumberOfLines-TrainingEntries
+    precentage = precentage / 100
+    TrainingEntries = (NumberOfLines * precentage)
+    TestingEntries = NumberOfLines - TrainingEntries
 
-    print ("the number of lines that will be used for training is  %d \nand the numbers used as a test will be %d" % (TrainingEntries, TestingEntries))
+    print("the number of lines that will be used for training is  %d \nand the numbers used as a test will be %d" % (
+        TrainingEntries, TestingEntries))
 
 
     #writes file into 2 seperate files, 1 for training and one for testing
-    counter =0
+    counter = 0
     for line in file:
-        if(counter<TrainingEntries):
+        if (counter < TrainingEntries):
             training.write(line)
             training.write("\n")
-            counter= counter+1
+            counter = counter + 1
 
         else:
             test.write(line)
@@ -95,22 +98,23 @@ def splitfile(file):
     test.close()
     #end of fxn split file
 
+
 def formatfiletrain():
     try:
         #declare 2 files - training and testing
-        training = open("trainfile.txt","r")#opens a file called trainfile.txt in read mode
+        training = open("trainfile.txt", "r")  #opens a file called trainfile.txt in read mode
     except IOError as etraining:
-        print(etraining)#gives error to user if cant open
+        print(etraining)  #gives error to user if cant open
         quit()
 
 
     #place all people in training into a file called training over 50k if over 50k , else put in a file under equal 50k
     try:
-        over50k=open("trainover50k.txt","w")
+        over50k = open("trainover50k.txt", "w")
     except IOError as openerrorover50k:
         print(openerrorover50k)
     try:
-        lesseq50k=open("trainlesseq50k.txt","w")
+        lesseq50k = open("trainlesseq50k.txt", "w")
     except IOError as openerrorundereq50k:
         print(openerrorundereq50k)
 
@@ -126,6 +130,7 @@ def formatfiletrain():
     lesseq50k.close()
     #end of fxn
 
+
 def adveragefxn(listlayout, search):
     #This fxn will recieve the list of how the data is arraged, The call will also send what it wants
     #to find the advearge of.
@@ -133,19 +138,18 @@ def adveragefxn(listlayout, search):
 
     #opens the 2 files
     try:
-        over50k=open("trainover50k.txt","r")
+        over50k = open("trainover50k.txt", "r")
     except IOError as openerrorover50k:
         print(openerrorover50k)
     try:
-        lesseq50k=open("trainlesseq50k.txt","r")
+        lesseq50k = open("trainlesseq50k.txt", "r")
     except IOError as openerrorundereq50k:
         print(openerrorundereq50k)
 
     #find out what the fxn wants to find the adverage of
     for item in listlayout:
         if item == search:
-            numberlookup = listlayout.index(item)#gives item number in list
-
+            numberlookup = listlayout.index(item)  #gives item number in list
 
     searchcounter = 0
 
@@ -156,9 +160,9 @@ def adveragefxn(listlayout, search):
 
 
     #over 50k file
-    adverageover50k =0
-    adveragevalueover50k=0
-    adveragecounter=0
+    adverageover50k = 0
+    adveragevalueover50k = 0
+    adveragecounter = 0
 
     #___________________________________________________________________
 
@@ -167,99 +171,115 @@ def adveragefxn(listlayout, search):
         searchcounter = 0
 
         for number in line.split(","):
-            number=number.lstrip()
+            number = number.lstrip()
 
-            if searchcounter>13:
-                searchcounter=0
+            if searchcounter > 13:
+                searchcounter = 0
 
             if number.isdigit():
-                if searchcounter==numberlookup:
+                if searchcounter == numberlookup:
                     adveragevalueover50k = adveragevalueover50k + int(number)
-                    adveragecounter=adveragecounter+1
+                    adveragecounter = adveragecounter + 1
                 else:
-                    searchcounter=searchcounter+1
+                    searchcounter = searchcounter + 1
             else:
-                searchcounter=searchcounter+1
+                searchcounter = searchcounter + 1
 
-
-
-    adverageover50k = adveragevalueover50k/adveragecounter
+    adverageover50k = adveragevalueover50k / adveragecounter
     print(adverageover50k)
     #____________________________________________________________
     #split file after every comma on file trainundereq50k..
-    adverageundereq50k=0
-    adveragevalueundereq50k=0
-    AdverageTotal=0
-    adveragecounter=0
+    adverageundereq50k = 0
+    adveragevalueundereq50k = 0
+    AdverageTotal = 0
+    adveragecounter = 0
 
     for line in lesseq50k:
 
         for number in line.split(","):
 
-            number=number.lstrip()
+            number = number.lstrip()
 
-            if searchcounter>13:
-                searchcounter=0
+            if searchcounter > 13:
+                searchcounter = 0
 
             if number.isdigit():
 
-                if searchcounter==numberlookup:
-                    adveragevalueundereq50k =adveragevalueundereq50k + int(number)
-                    adveragecounter=adveragecounter+1
+                if searchcounter == numberlookup:
+                    adveragevalueundereq50k = adveragevalueundereq50k + int(number)
+                    adveragecounter = adveragecounter + 1
                 else:
-                    searchcounter=searchcounter+1
+                    searchcounter = searchcounter + 1
             else:
-                searchcounter=searchcounter+1
+                searchcounter = searchcounter + 1
 
-
-
-
-
-
-    adverageundereq50k = adveragevalueundereq50k/adveragecounter
+    adverageundereq50k = adveragevalueundereq50k / adveragecounter
     print(adverageundereq50k)
 
     #adverage of adverage
-    AdverageTotal = (adverageover50k+adverageundereq50k )/2
+    AdverageTotal = (adverageover50k + adverageundereq50k ) / 2
     print(AdverageTotal)
 
-    return(AdverageTotal)
+    return (AdverageTotal)
 
 
-def discrete_Cal(listlayout ,searchbn):
+def discrete_Cal(listlayout,listusing,  search):
+
+    try:
+        over50k = open("trainover50k.txt", "r")
+    except IOError as openerrorover50k:
+        print(openerrorover50k)
+
+    try:
+        lesseq50k = open("trainlesseq50k.txt", "r")
+    except IOError as openerrorundereq50k:
+        print(openerrorundereq50k)
+
     # number look up
-        for item in listlayout:
-          if item == search:
-              numberlookup = listlayout.index(item)#gives item number in list
-
-
-        
+    for item in listlayout:
+        if item == search:
+            numberlookup = listlayout.index(item)  #gives item number in list
 
 
 
 #fnlwgt education and native country not needed for study
-listlayout = ["age","workclass","fnlwgt","education","education-no","marital-status","occupation","relationship","race","sex","capital-gain","capital-loss","hours-per-week","native-country","income"]
-file=downloadfile()
+listlayout = ["age", "workclass", "fnlwgt", "education", "education-no", "marital-status", "occupation", "relationship",
+              "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "income"]
+
+
+
+
+file = downloadfile()
 splitfile(file)
 formatfiletrain()
 #get adverage age on both over and lesseq files
-adverag_age = adveragefxn(listlayout ,"age") #sends list and string called age
+adverag_age = adveragefxn(listlayout, "age")  #sends list and string called age
 #get adverage education-number on over and lesseq files
-adverage_ed_no = adveragefxn(listlayout,"education-no")
-adverage_capital_gain = adveragefxn(listlayout,"capital-gain")
-adverage_capital_loss = adveragefxn(listlayout,"capital-loss")
-adverage_Hours_per_week = adveragefxn(listlayout,"hours-per-week")
+adverage_ed_no = adveragefxn(listlayout, "education-no")
+adverage_capital_gain = adveragefxn(listlayout, "capital-gain")
+adverage_capital_loss = adveragefxn(listlayout, "capital-loss")
+adverage_Hours_per_week = adveragefxn(listlayout, "hours-per-week")
 
 #-----------------
-#lists
-list_workclass = ["Private","Self-emp-not-inc","Self-emp-inc","Federal-gov","Local-gov","State-gov","Without-pay","Never worked"]
-list_marital_status =["Married-civ-spouse","Divorced","Never-Married","separated","Widowed","Married-spouse-absent"]
-list_job =["Tech-support", "Craft-repair", "Other-service", "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners", "Machine-op-inspct", "Adm-clerical", "Farming-fishing", "Transport-moving", "Priv-house-serv", "Protective-serv", "Armed-Forces"]
-list_relationship = [ "Wife", "Own-child", "Husband", "Not-in-family", "Other-relative", "Unmarried"]
-list_race =["White", "Asian-Pac-Islander","Amer-Indian-Eskimo", "Other", "Black"]
-list_gender =["Male","Female"]
+#dictionarys define
+
+workclass_over50k{}
+workclass_undereq50{}
+matital-status_over50k{}
+matital-status_undereq50k{}
+occupation_over50k{}
+occupation_undereq50k{}
+relationship_over50k{}
+relationship_under50k{}
+race_over50k{}
+race_undereq50k{}
+sex_over50k{}
+sex_undereq50k{}
+ w
+
+
 #------------
-workclass_over50k =discrete_Cal(listlayout,"workclass")
+workclass = discrete_Cal(listlayout,list_workclass "workclass")
 
 
 
